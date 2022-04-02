b5fd20556c52216d2fae231ac71fd12e0e3bb571
Aldrin Sy <aldrin09176@yahoo.com>
2022-04-02 11:31:06 +0800

    1st commmit

---
+++ sample_teresa_project/src/sample_teresa_project/hello_world.java
@@ -0,0 +1,10 @@
+package sample_teresa_project;
+
+public class hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("geeheh");
+	}
+
+}
